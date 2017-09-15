76eb6c11e92920809f5dd900c2cad4c04f661df5
BigPapaDoty <BigPapaDoty@10.84.125.55>
2017-09-15 14:19:23 -0400

    new test class

---
+++ src/edu/buffalo/cse116/MainTest.java
@@ -0,0 +1,19 @@
+package edu.buffalo.cse116;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
