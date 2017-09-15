33d7c3f4eb489d07d8bd0afbb9fd128d7ef326a6
BigPapaDoty <BigPapaDoty@10.84.125.55>
2017-09-15 14:18:57 -0400

    hello

--- src/edu/buffalo/cse116/Main.java
+++ src/edu/buffalo/cse116/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+	  //hello everyone my name is patrick
   }
 
-}
-public class hello{
-	
 }
