0b1827a27ce5cfcf425bd01200ae5c961617cd2e
akshit792 <akshit792@gmail.com>
2017-09-27 21:55:26 -0400

    some classes pushed

--- src/edu/buffalo/cse116/DeckOfCards.java
+++ src/edu/buffalo/cse116/DeckOfCards.java
@@ -1,9 +1,51 @@
 package edu.buffalo.cse116;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 public class DeckOfCards {
-	//private 
-	public DeckOfCards() {
-		// TODO Auto-generated constructor stub
+	private ArrayList<cards> cards;
+
+	public DeckOfCards()
+
+	{
+		Random generator = new Random();
+		cards = new ArrayList<cards>();
+		int value1;
+		int value2;
+		cards variable;
+
+		for (int suit = 0; suit <= 3; suit++) {
+			for (int rank = 0; rank <= 12; rank++) {
+				cards.add(new cards(suit, rank));
+			}
+		}
+		for (int i = 0; i < 100; i++) {
+			value1 = generator.nextInt(cards.size() - 1);
+			value2 = generator.nextInt(cards.size() - 1);
+
+			variable = (cards) cards.get(value2);
+			cards.set(value2, cards.get(value1));
+			cards.set(value1, variable);
+		}
+	}
+
+	public cards drawFromDeck() {
+		return cards.remove(0);
+	}
+
+	public int getTotalCards() {
+		return cards.size();
+
+	}
+
+	public static void main(String[] args) {
+		DeckOfCards deck = new DeckOfCards();
+		cards test;
+		while (deck.getTotalCards() > 0) {
+			test = deck.drawFromDeck();
+			System.out.println(test.toString());
+		}
 	}
 
 }
--- src/edu/buffalo/cse116/cards.java
+++ src/edu/buffalo/cse116/cards.java
@@ -1,9 +1,25 @@
 package edu.buffalo.cse116;
 
 public class cards {
-
-	public cards() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
+	
+	private int suit;
+	private int rank;
+	
+	 private static String[] suits = { "Clubs","Diamons","Hearts","Spades" };
+	 private static String[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+	 
+	 public cards(int suit, int rank){
+	         this.rank=rank;
+	         this.suit=suit;
+	 }
+		public String toString(){
+	           return ranks[rank] + " of " + suits[suit];
+	     }
+	     public int getRank() {
+	          return rank;
+	     }
+	     public int getSuit() {
+	         return suit;
+	     }
+	 
+  }
