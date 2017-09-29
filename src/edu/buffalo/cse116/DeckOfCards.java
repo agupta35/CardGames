cb9786b240d93e4069841c1d66c64e9091c8fbd9
BigPapaDoty <BigPapaDoty@10.84.70.232>
2017-09-29 14:15:36 -0400

    COmpletely finished deckofcards

--- src/edu/buffalo/cse116/DeckOfCards.java
+++ src/edu/buffalo/cse116/DeckOfCards.java
@@ -9,43 +9,52 @@ public class DeckOfCards {
 	public DeckOfCards()
 
 	{
-		Random generator = new Random();
+		/**
+		 * These are variables that will be used later on.
+		 */
+		Random num = new Random();
 		cards = new ArrayList<cards>();
 		int value1;
 		int value2;
-		cards variable;
-
-		for (int suit = 0; suit <= 3; suit++) {
-			for (int rank = 0; rank <= 12; rank++) {
-				cards.add(new cards(suit, rank));
+		cards cardVariable;
+		
+		/**This for loop runs the 4 suits and has another for loop 
+		 * that puts the 12 different ranks into each suit and then 
+		 * takes those values and puts them into the card class to create
+		 * a new cards object
+		 */
+		for (int suit = 0; suit < 4; suit++) {
+			for (int rank = 0; rank < 13; rank++) {
+				String color = null;
+				cards.add(new cards(suit, rank, color));
 			}
 		}
-		for (int i = 0; i < 100; i++) {
-			value1 = generator.nextInt(cards.size() - 1);
-			value2 = generator.nextInt(cards.size() - 1);
+		/**Runs a for loop 52 times to create a new card and decreases the deck size by 1
+		 * that same card from the deck so that we don't repeat any of the same cards
+		 */
+		for (int i = 0; i < 52; i++) {
+			value1 = num.nextInt(cards.size() - 1);
+			value2 = num.nextInt(cards.size() - 1);
 
-			variable = (cards) cards.get(value2);
+			cardVariable = (cards) cards.get(value2);
 			cards.set(value2, cards.get(value1));
-			cards.set(value1, variable);
+			cards.set(value1, cardVariable);
 		}
 	}
-
-	public cards drawFromDeck() {
+	/**Draws a card from the deck and removes it*/
+	public cards getACard() {
 		return cards.remove(0);
 	}
-
+	/**Gets the total size of the deck*/
 	public int getTotalCards() {
 		return cards.size();
 
 	}
-
+	/**In class test to make sure the deck return 52 random and non-repeated cards*/
 	public static void main(String[] args) {
 		DeckOfCards deck = new DeckOfCards();
-		cards test;
-		while (deck.getTotalCards() > 0) {
-			test = deck.drawFromDeck();
-			System.out.println(test.toString());
-		}
+		for(int i=0;i<52;i++){
+		System.out.println(deck.getACard());}
 	}
 
 }
--- src/edu/buffalo/cse116/cards.java
+++ src/edu/buffalo/cse116/cards.java
@@ -1,25 +1,61 @@
 package edu.buffalo.cse116;
 
 public class cards {
-	
+	/**
+	 * Instance variables for color, suit, and rank
+	 */
 	private int suit;
 	private int rank;
+	private String color;
 	
-	 private static String[] suits = { "Clubs","Diamons","Hearts","Spades" };
+	/**
+	 * Two array instances with all the suits and all the ranks.
+	 */
+	 private static String[] suits = { "Clubs","Diamonds","Hearts","Spades" };
 	 private static String[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 	 
-	 public cards(int suit, int rank){
+	 /**
+	  * Card Constructor
+	  * @param suit
+	  * @param rank
+	  * @param color
+	  */
+	 public cards(int suit, int rank, String color){
 	         this.rank=rank;
 	         this.suit=suit;
+	         this.color=color;
 	 }
+	 /**
+	  * @return A string giving the color, rank, and suit
+	  */
 		public String toString(){
-	           return ranks[rank] + " of " + suits[suit];
+			if(suits[suit].equals("Diamonds") || suits[suit].equals("Hearts")){
+				color = "Red";
+	           return color+ ": " + ranks[rank] + " of " + suits[suit];
+			} else {
+				color = "Black";
+			return color+ ": " + ranks[rank] + " of " + suits[suit];
+			}
 	     }
+		/**
+		 * @return the rank for the given card
+		 */
 	     public int getRank() {
 	          return rank;
 	     }
+	     /**
+		 * @return the suit for the given card
+		 */
 	     public int getSuit() {
 	         return suit;
 	     }
+	     /**
+		 * @return the color for the given card
+		 */
+	     public String getColor(){
+	    	 return color;
+	     }
+	     
 	 
   }
+
