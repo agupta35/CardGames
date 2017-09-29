33f8ada6d8cbb910ff37fe5f7cd2c0b664335929
akshit792 <akshit792@gmail.com>
2017-09-29 14:45:18 -0400

    Tableau pile

--- src/edu/buffalo/cse116/TableauPileFC.java
+++ src/edu/buffalo/cse116/TableauPileFC.java
@@ -1,5 +1,7 @@
 package edu.buffalo.cse116;
 
+import java.util.ArrayList;
+
 public class TableauPileFC{
 	
 	private cards _card1;
@@ -12,73 +14,81 @@ public class TableauPileFC{
 	
 	//one in front is card7, one in the back is card1
 	
-	public static cards[] tableauPile = new cards[52];
+	public static ArrayList<cards> tableauPile = new ArrayList<cards>();
 	public static cards theRemovedCard;
 	
 	public TableauPileFC(cards card1, cards card2, cards card3, cards card4,cards card5,cards card6,cards card7){
 		
-        this._card1 = card1;
-        tableauPile[0] = _card1;
+		this._card1 = card1;
+        tableauPile.add(_card1);
         
         this._card2 = card2;
-        tableauPile[1] = _card2;
+        tableauPile.add(_card2);
         
         this._card3 = card3;
-        tableauPile[2] = _card3;
+        tableauPile.add(_card3);
         
         this._card4 = card4;
-        tableauPile[3] = _card4;
+        tableauPile.add(_card4);
         
         this._card5 = card5;
-        tableauPile[4] = _card5;
+        tableauPile.add(_card5);
         
         this._card6 = card6;
-        tableauPile[5] = _card6;
+        tableauPile.add(_card6);
         
         this._card7 = card7;
-        tableauPile[6] = _card7;
+        tableauPile.add(_card7);
 	}
 public TableauPileFC(cards card1, cards card2, cards card3, cards card4,cards card5,cards card6){
 		
-        this._card1 = card1;
-        tableauPile[0] = _card1;
+	this._card1 = card1;
+    tableauPile.add(_card1);
+    
+    this._card2 = card2;
+    tableauPile.add(_card2);
+    
+    this._card3 = card3;
+    tableauPile.add(_card3);
+    
+    this._card4 = card4;
+    tableauPile.add(_card4);
         
-        this._card2 = card2;
-        tableauPile[1] = _card2;
-        
-        this._card3 = card3;
-        tableauPile[2] = _card3;
-        
-        this._card4 = card4;
-        tableauPile[3] = _card4;
-        
-        this._card5 = card5;
-        tableauPile[4] = _card5;
-        
-        this._card6 = card6;
-        tableauPile[5] = _card6;
+    this._card5 = card5;
+    tableauPile.add(_card5);
+    
+    this._card6 = card6;
+    tableauPile.add(_card6);
+    
+    
         
       
 	}
+
+public ArrayList<cards> getTableauArrayList(){
+	return tableauPile;
+}
 	
-	public boolean addCard(cards addCard){
-		for(int i = 0; i < tableauPile.length; i++){
-			if(tableauPile[i] == null && tableauPile[i-1].getRank() == addCard.getRank()+1){
-				tableauPile[i] = addCard;
-				return true;
-			}
-		}
+public boolean addCard(cards addCard){
+	if(tableauPile.get(0) == null){
 		return false;
 	}
-	
-	public boolean removingTopCard(){
-		for(int i = 0; i < tableauPile.length; i++){
-			if(tableauPile[i] == null){ 				//This is the first spot in the array where there is no card
-				theRemovedCard = tableauPile[i-1];		//The index previous to it is where the top card is and it is stored in theRemovedCard
-				tableauPile[i-1] = null;				//Then by setting that index to null it no longer is in the pile
-				return true;
-			}											
-		}
+	else if(tableauPile.get(tableauPile.size()-1).getRank() == addCard.getRank()+1){
+		tableauPile.add(addCard);
+		return true;
+	}
+	else{
 		return false;
-	}	
+	}
+}
+
+public boolean removingTopCard(){
+	if(tableauPile.get(0) == null){
+		return false;
+	}
+	else{
+		tableauPile.remove(tableauPile.size()-1);
+		return true;
+	}
+}	
 }
